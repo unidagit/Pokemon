a78e01b09ebf77cdd839dd705ef463c2bb90b3d7
yunheejo <rhcjrdbsgml@gmail.com>
2022-12-02 17:33:18 +0900

    초기 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/navbar/Navbar";
+import TopButton from "./components/topButton/TopButton";
+import Home from "./pages/Home/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <TopButton />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pokemonSearchCard/PokemonSearchCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PokemonSearchCard() {
+  return <div>PokemonSearchCard</div>;
+}
+
+export default PokemonSearchCard;
