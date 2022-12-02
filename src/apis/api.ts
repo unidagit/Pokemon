cd23d9b58adfffac3385f74409732aab468db8e3
yunheejo <rhcjrdbsgml@gmail.com>
2022-12-02 18:01:08 +0900

    getPokemon 리액트 쿼리 통신

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,15 @@
 const BASE_PATH = "https://pokeapi.co/api/v2";
+const LIMIT = 100;
+const OFFSET = 0;
 
 export function getSearchPokemon(pokemonName: string) {
   return fetch(`${BASE_PATH}/pokemon/${pokemonName}`).then((response) =>
     response.json()
+  );
+}
+
+export function getPokemon() {
+  return fetch(`${BASE_PATH}/pokemon?limit=${LIMIT}&offset=${OFFSET}`).then(
+    (response) => response.json()
   );
 }
