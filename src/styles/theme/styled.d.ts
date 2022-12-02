ca5464ed8c036040b87b5428cf4b7d294bea60ad
yunheejo <rhcjrdbsgml@gmail.com>
2022-12-02 17:34:59 +0900

    css 스타일 설정

---
+++ src/styles/theme/styled.d.ts
@@ -0,0 +1,22 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    red: {
+      basic: string;
+      lighter: string;
+    };
+    black: {
+      veryDark: string;
+      darker: string;
+      lighter: string;
+    };
+    white: {
+      darker: string;
+      lighter: string;
+    };
+    yellow: {
+      basic: string;
+    };
+  }
+}
