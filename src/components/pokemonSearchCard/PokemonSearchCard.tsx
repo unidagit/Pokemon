d9272c4a685e0219430a61fbc2c5c4e86b1e9328
yunheejo <rhcjrdbsgml@gmail.com>
2022-12-02 17:58:46 +0900

    검색 기능 생성

--- src/components/pokemonSearchCard/PokemonSearchCard.tsx
+++ src/components/pokemonSearchCard/PokemonSearchCard.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import {
+  CardContainer,
+  CardInfo,
+  CardItem,
+  ItemImg,
+  ItemName,
+  ItemNumber,
+} from "../../styles/commonStyle";
 
-function PokemonSearchCard() {
-  return <div>PokemonSearchCard</div>;
+interface IPokemon {
+  id: number;
+  name: string;
+  image: string;
+}
+
+function PokemonSearchCard({ name, id, image }: IPokemon) {
+  return (
+    <CardContainer>
+      <CardItem>
+        <CardInfo>
+          <ItemNumber>{name}</ItemNumber>
+          <ItemName># {id}</ItemName>
+        </CardInfo>
+        <ItemImg src={image} alt={name} />
+      </CardItem>
+    </CardContainer>
+  );
 }
 
 export default PokemonSearchCard;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,16 +4,17 @@ import styled from "styled-components";
 import { getSearchPokemon } from "../../apis/api";
 import SearchBar from "../../components/searchbar/SearchBar";
 import { IDetailPokemon } from "../../interfaces/interface";
+import PokemonSearchCard from "../../components/pokemonSearchCard/PokemonSearchCard";
 
 function Home() {
-  const [pokemonName, setPokemonName] = useState("");
-  const [value, setValue] = useState("");
+  const [pokemonName, setPokemonName] = useState(""); //서버에 보낼 이름
+  const [value, setValue] = useState(""); //검색창에 적히는 문자
 
   //검색하면 나오는 데이터
-  const { data: pokeSearchData } = useQuery<IDetailPokemon>(
-    ["pokemon", pokemonName],
-    () => getSearchPokemon(`${pokemonName}`)
-  );
+  const { data: pokeSearchData, isLoading: isSearchLoading } =
+    useQuery<IDetailPokemon>(["pokemon", pokemonName], () =>
+      getSearchPokemon(`${pokemonName}`)
+    );
   console.log(pokeSearchData);
 
   const searchOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -37,6 +38,19 @@ function Home() {
           onReset={onReset}
           onSubmit={searchOnSubmit}
         />
+        {isSearchLoading ? (
+          <Loader>포켓몬 찾는중...</Loader>
+        ) : pokeSearchData === undefined ? (
+          <Loader>{pokemonName}에 대한 검색 결과가 없습니다.</Loader>
+        ) : (
+          <SearchResult>
+            <PokemonSearchCard
+              id={pokeSearchData.id}
+              name={pokeSearchData.name}
+              image={pokeSearchData.sprites?.front_default}
+            />
+          </SearchResult>
+        )}
       </Container>
     </Wrapper>
   );
@@ -55,3 +69,14 @@ const Container = styled.div`
   flex-direction: column;
   margin: 80px;
 `;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const SearchResult = styled.div``;
