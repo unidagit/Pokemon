9d3779f465c06caac3a9c66736d33fc617cef5d1
yunheejo <rhcjrdbsgml@gmail.com>
2022-12-02 17:42:49 +0900

    input 스타일링

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import styled from "styled-components";
+import { ISearch } from "../../interfaces/interface";
 
-function SearchBar() {
-  return <div>SearchBar</div>;
+function SearchBar({ onChange, value, onSubmit, onReset }: ISearch) {
+  const searchOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit();
+    onReset();
+  };
+
+  return (
+    <Container onSubmit={searchOnSubmit}>
+      <SearchInput
+        type="text"
+        onChange={onChange}
+        value={value}
+        placeholder="포켓몬 이름 또는 번호를 검색해보세요."
+      />
+      <SearchButton type="submit" onClick={onSubmit}>
+        Search
+      </SearchButton>
+    </Container>
+  );
 }
 
 export default SearchBar;
+
+const Container = styled.form`
+  display: flex;
+  align-items: center;
+  margin: 10px 0 50px 0;
+  background-color: ${(props) => props.theme.white.lighter};
+  border-radius: 5rem;
+  border: 3px solid ${(props) => props.theme.black.darker};
+  height: 50px;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  border: none;
+  background-color: transparent;
+  outline: none;
+  padding: 0.8rem;
+`;
+
+const SearchButton = styled.button`
+  padding: 0.8rem;
+  border: none;
+  background-color: transparent;
+  font-size: 1rem;
+  font-weight: bold;
+  color: ${(props) => props.theme.black.darker};
+  cursor: pointer;
+  &:hover {
+    color: ${(props) => props.theme.yellow.basic};
+  }
+`;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import SearchBar from "../../components/searchbar/SearchBar";
 
 function Home() {
-  return <div>Home</div>;
+  const [pokemonName, setPokemonName] = useState("");
+  const [value, setValue] = useState("");
+
+  const searchOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const searchOnSubmit = () => {
+    setPokemonName(value);
+  };
+
+  const onReset = () => {
+    setValue("");
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <SearchBar
+          onChange={searchOnChange}
+          value={value}
+          onReset={onReset}
+          onSubmit={searchOnSubmit}
+        />
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default Home;
+
+const Wrapper = styled.div`
+  min-width: 550px;
+  max-width: 850px;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 80px;
+`;
